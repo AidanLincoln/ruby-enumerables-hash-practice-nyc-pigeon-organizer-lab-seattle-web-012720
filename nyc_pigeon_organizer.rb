21fd060da72683407ebca2e7693ed9adb2d7b8f3
Aidan Lincoln <aidantlincoln@gmail.com>
2020-01-13 00:08:56 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,6 +1,7 @@
 def nyc_pigeon_organizer(data)
   new_hash = {}
   data[:color][:purple].map do |pigeon|
-    new_hash[pigeon] = 
+    new_hash[pigeon] = :color => "purple"
   end
+  pp new_hash
 end
