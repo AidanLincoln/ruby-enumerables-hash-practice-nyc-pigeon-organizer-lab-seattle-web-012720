8c3201598ccfd11bce810c6b16ef59618995b67e
Aidan Lincoln <aidantlincoln@gmail.com>
2020-01-13 01:26:29 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,60 +1,18 @@
 def nyc_pigeon_organizer(data)
   new_hash = {}
- 
-  data[:color][:purple].map do |pigeon|
-    if !new_hash[pigeon]
-      new_hash[pigeon] = {:color => ["purple"]}
-    else
-      new_hash[pigeon][:color] << "purple"
+  data.each do |color_gen_loc, data_hash|
+    data_hash.each do |attribute, array|
+      array.each do |name|
+        if !new_hash.has_key?(name)
+          new_hash[name] = {}
+        end
+        if !new_hash[name].has_key?(color_gen_loc)
+          new_hash[name][color_gen_loc] = []
+        end
+        if !new_hash[name][color_gen_loc].include?(attribute)
+          new_hash[name][color_gen_loc] << attribute
+        end
+      end
     end
   end
-  
-  data[:color][:grey].map do |pigeon|
-    if !new_hash[pigeon]
-      new_hash[pigeon] = {:color => ["grey"]}
-    else
-      new_hash[pigeon][:color] << "grey"
-    end
-  end
-  
-  data[:color][:white].map do |pigeon|
-    if !new_hash[pigeon]
-      new_hash[pigeon] = {:color => ["white"]}
-    else
-      new_hash[pigeon][:color] << "white"
-    end
-  end
-  
-  data[:color][:brown].map do |pigeon|
-    if !new_hash[pigeon]
-      new_hash[pigeon] = {:color => ["brown"]}
-    else
-      new_hash[pigeon][:color] << "brown"
-    end
-  end
-  
-  data[:gender][:male].map do |pigeon|
-    new_hash[pigeon][:gender] = ["male"]
-  end
-  
-  data[:gender][:female].map do |pigeon|
-    new_hash[pigeon][:gender] = ["female"]
-  end
-  
-  data[:lives]["Subway"].map do |pigeon|
-    new_hash[pigeon][:lives] = ["Subway"]
-  end
-  
-   data[:lives]["Central Park"].map do |pigeon|
-    new_hash[pigeon][:lives] = ["Central Park"]
-  end
-  
-   data[:lives]["Library"].map do |pigeon|
-    new_hash[pigeon][:lives] = ["Library"]
-  end
-  
-   data[:lives]["City Hall"].map do |pigeon|
-    new_hash[pigeon][:lives] = ["City Hall"]
-  end
-  pp new_hash
 end
